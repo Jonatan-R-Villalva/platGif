4f8be37ac630495c54e0d7a4a4e6669e4cddaecc
= <=>
2023-04-19 20:13:32 -0300

    creando custom hook y exportando a searchresults

--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -1,21 +1,11 @@
-import {useEffect, useState} from 'react';
 import Spinner from '../../components/Spinner/Spinner';
 import ListOfGifs from '../../components/ListOfGifs/ListOfGifs';
-import getGifs from '../../sevices/getGifs';
+import { useGifs } from '../../hooks/UseGifs';
 
 export default function SearchResults({params}){
     const { keyword } = params;
-    const [loading, setLoading] = useState(false);
-    const [gifs, setGifs] = useState([]);
-
-    useEffect(()=>{
-        setLoading(true);
-        getGifs({keyword})
-            .then(gifs =>{
-                setGifs(gifs)
-                setLoading(false)
-            })
-    },[keyword])
+    const { loading, gifs} = useGifs({keyword})
+    
 
     return <>
     {
